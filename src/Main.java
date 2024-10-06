39152843bf217420a6fcc45abdccc67676b65b8d
Roman Bychkov <RBychkov@nota.tech>
2024-10-06 15:47:56 +0300

    Внес изменения в класс main по заданию 1 части 4 блока 9 - обработка исключений

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Введите первое число:");
         int firstNumber = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число:");
@@ -27,6 +32,7 @@ public class Main {
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirecory = file.isDirectory();
+            ArrayList<Integer> lengths = new ArrayList<Integer>();
 
             if (!fileExists || isDirecory ) {
                 System.out.println("Указанный файл не существует или указанный путь является путём к папке");
@@ -35,6 +41,23 @@ public class Main {
                 System.out.println("Путь указан верно");
                 count++;
                 System.out.println("Это файл номер " + count);
+            }
+
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader =
+                        new BufferedReader(fileReader);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    if (length > 1024) throw new RuntimeException("В файле имеется строка длиннее 1024 символа. Таких длинных строк в файле быть не должно.");
+                    lengths.add(length);
+                }
+                System.out.println("Общее колличество строк в файле: " + lengths.size());
+                System.out.println("Длинна самой длинной строки в файле: " + Collections.max(lengths));
+                System.out.println("Длинна самой короткой строки в файле: " + Collections.min(lengths));
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
 
